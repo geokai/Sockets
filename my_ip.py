2cdfd5b59ee569f218c681417fc8a909de2f2870
George Kaimakis <geoptus@gmail.com>
2020-12-03 00:07:41 +0000

    remove redundent code line:

--- my_ip.py
+++ my_ip.py
@@ -5,6 +5,5 @@ response = requests.get('https://httpbin.org/ip')
 # print(response.json())
 ip = response.json()['origin'].split(',')
 
-# print('Your IP is {0}'.format(response.json()['origin']))
 # 'ip' is a list object (due to the .split() method)
 print('Your IP address is {0}'.format(ip[0]))
